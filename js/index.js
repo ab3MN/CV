99a2c4c38a8b013de527e939546766aaf63dd013
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2022-02-22 07:33:01 +0200

    done

---
+++ js/index.js
@@ -0,0 +1,39 @@
+'use strict';
+const isDark = localStorage.getItem('isDarkTheme');
+const onDarkTheme = () => (
+  themeButton.classList.add('bx-sun'), root.classList.add('dark-theme')
+);
+isDark && onDarkTheme();
+
+const on = () => (onDarkTheme(), localStorage.setItem('isDarkTheme', true));
+const off = () => {
+  themeButton.classList.remove('bx-sun');
+  root.classList.remove('dark-theme');
+  localStorage.removeItem('isDarkTheme');
+};
+
+themeButton.onclick = () =>
+  localStorage.getItem('isDarkTheme') === null ? on() : off();
+
+navToggle.onclick = () => navMenu.classList.toggle('show-menu');
+
+navMenu.onclick = () => navMenu.classList.remove('show-menu');
+
+// const opt = {
+//   margin: 1,
+//   filename: 'Front-End(Mykhailo-Nikolaiev).pdf',
+//   image: { type: 'jpeg', quality: 0.98 },
+//   html2canvas: { scale: 4 },
+//   jsPDF: { format: 'a4', orientation: 'portrait' },
+// };
+
+// const onDownload = () => {
+//   root.classList.add('pdf');
+//   setTimeout(() => root.classList.remove('pdf'));
+
+//   html2pdf(cv, opt);
+// };
+
+// document
+//   .querySelectorAll('.download')
+//   .forEach((el) => (el.onclick = onDownload));
